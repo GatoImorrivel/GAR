867b2d904482df49d12dda66887b908f0f8eb39a
GatoImorrivel <70108109+GatoImorrivel@users.noreply.github.com>
2021-08-27 20:50:53 -0300

    another tweak

--- install-on-arch.sh
+++ install-on-arch.sh
@@ -22,7 +22,7 @@ rustup default stable
 git clone https://github.com/alacritty/alacritty.git ~
 cd ~/alacritty
 
-cargo install --release
+cargo build --release
 sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
