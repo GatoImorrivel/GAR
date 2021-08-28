0ffceb553b1855dfb5cb9106889ff54893bcb7d5
GatoImorrivel <70108109+GatoImorrivel@users.noreply.github.com>
2021-08-27 22:49:01 -0300

    should work idk

--- install-on-arch.sh
+++ install-on-arch.sh
@@ -4,7 +4,7 @@ sudo pacman --noconfirm -Syu
 
 # Install dependecies
 echo "Installing a bunch of stuff"
-sudo pacman -S --noconfirm --needed xorg xorg-xinit bspwm sxhkd lightdm lightdm-gtk-greeter git python3 gcc
+sudo pacman -S --noconfirm --needed xorg xorg-xinit bspwm sxhkd lightdm lightdm-gtk-greeter git python3 gcc base-devel
 
 # Setup lightdm
 sudo systemctl enable lightdm
@@ -20,16 +20,14 @@ sudo pacman -S --needed rustup cargo
 
 rustup default stable
 
-# Clone and setup alacritty
-mkdir ~/alacritty
-git clone https://github.com/alacritty/alacritty.git ~/alacritty
-cd ~/alacritty
+# Installing paru
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+paru -Syu
 
-cargo build --release
-sudo cp target/release/alacritty /usr/local/bin
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
+# Installing alacritty
+paru -S alacritty
 
 # Copying BSPWM, SXHKD and NVIM configs
 mkdir ~/.config
@@ -37,8 +35,8 @@ mkdir ~/.config
 mkdir ~/.config/bspwm/
 mkdir ~/.config/sxhkd/
 
-sudo cp -r ~/GAR/config/bspwm/bspwmrc ~/.config/bspwm/
-sudo cp -r ~/GAR/config/sxhkd/sxhkdrc ~/.config/sxhkd/
+sudo cp -r ~/GAR/configs/bspwm/bspwmrc ~/.config/bspwm/
+sudo cp -r ~/GAR/configs/sxhkd/sxhkdrc ~/.config/sxhkd/
 
 # Copying xinit and xprofile
 sudo cp ~/GAR/xconfigs/.xinit ~/
