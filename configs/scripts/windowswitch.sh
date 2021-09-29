13112c7c3e965bf6a550d588c878ab6cd56829ad
GatoImorrivel <gsantos1510@gmail.com>
2021-09-29 02:45:09 -0300

    better windowswitch script

--- configs/scripts/windowswitch.sh
+++ configs/scripts/windowswitch.sh
@@ -1,59 +1,49 @@
-TARGETWORKSPACE=$1
-PREVINDEX=$(cat ~/.config/scripts/currentwindow)
+CURRENTWORKSPACEVARPREFIX="activeWorkspace: "
+PREVIOUSWORKSPACEVARPREFIX="previousWorkspace: "
+
+CURRENTWORKSPACE="$(eww state | grep "activeWorkspace")"
+CURRENTWORKSPACE="${CURRENTWORKSPACE#"$CURRENTWORKSPACEVARPREFIX"}"
+
+PREVIOUSWORKSPACE="$(eww state | grep "previousWorkspace")"
+PREVIOUSWORKSPACE="${PREVIOUSWORKSPACE#"$PREVIOUSWORKSPACEVARPREFIX"}"
+
+echo "$CURRENTWORKSPACE $PREVIOUSWORKSPACE"
+
+ACTION=$1
+TARGETWORKSPACE=$2
 WORKSPACEAMOUNT=7
 
-function update_windows () {
-  for i in $(seq 1 $WORKSPACEAMOUNT); do
-    if [ $i == $TARGETWORKSPACE ]; then
-      eww update workspace${i}="workspaceActive"
-    else
-      eww update workspace${i}="workspaceInactive"
-    fi
-  done
-  bspc desktop -f "$TARGETWORKSPACE"
-  echo $TARGETWORKSPACE > ~/.config/scripts/currentwindow
-  echo $PREVINDEX > ~/.config/scripts/previouswindow
+windowSwitch() {
+  bspc desktop -f $1
+  eww update activeWorkspace=$1
+  eww update previousWorkspace="${CURRENTWORKSPACE}"
 }
 
-# Handles changing to the workspace in front of the current workspace
-if [[ $TARGETWORKSPACE -gt $WORKSPACEAMOUNT ]]; then
-  echo "invalid target workspace"
-  exit 1
-fi
-
-if [ $TARGETWORKSPACE == '-n' ]; then
-  if [[ $PREVINDEX -ge $WORKSPACEAMOUNT ]]; then
-    CURRENTINDEX=1
-  else
-    CURRENTINDEX=$(( $PREVINDEX + 1))
+if [ $ACTION == '-c' ]; then
+  if [[ $TARGETWORKSPACE -gt $WORKSPACEAMOUNT ]] || [[ $TARGETWORKSPACE -lt 1 ]]; then
+    echo "Invalid target workspace"
+    exit 1
   fi
-  TARGETWORKSPACE=$CURRENTINDEX
-
-  update_windows 
-  
-  exit 2
 fi
 
-# Handles changing to the workspace behind the current workspace
-if [ $TARGETWORKSPACE == '-p' ]; then 
-  if [[ $PREVINDEX -le 1 ]]; then
-    CURRENTINDEX=7
-  else
-    CURRENTINDEX=$(( $PREVINDEX - 1))
-  fi
-  TARGETWORKSPACE=$CURRENTINDEX
 
-  update_windows 
-
-  exit 3
-fi
-
-# Handles changing to the last focused workspace
-if [ $TARGETWORKSPACE == '-l' ]; then
-  TARGETWORKSPACE=$(cat ~/.config/scripts/previouswindow)
-  update_windows
-fi
-
-# Default is just to update to target index
-update_windows 
-
+case $ACTION in
+  -n)
+    NEXT="$(( $CURRENTWORKSPACE + 1 ))"
+    windowSwitch $NEXT     
+    ;;
+  -b)
+    BEHIND="$(( $CURRENTWORKSPACE - 1 ))"
+    windowSwitch $BEHIND    
+    ;;
+  -l)
+    windowSwitch $PREVIOUSWORKSPACE
+    ;;
+  -c)
+    windowSwitch $TARGETWORKSPACE
+    ;;
+  *)
+    "Invalid flags"
+    exit 2
+    ;;
+esac
